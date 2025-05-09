4f00fb73a09bb62488235c1ee6d70b98f1c82ffa
Alexander Wang <98280966+AlexanderWangY@users.noreply.github.com>
2025-05-09 10:09:43 -0500

    build: add Docker compose and file for API development (#4)
    * created root readme
    
    * init air and dockerfile
    
    * added dockerfile, compose, and redis + postgres

--- apps/api/cmd/api/main.go
+++ apps/api/cmd/api/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("[GET] /")
+	fmt.Fprintf(w, "Hello!")
+}
 
 func main() {
-	fmt.Println("This is the backend entry point.")
+	http.HandleFunc("/", handler)
+
+	fmt.Println("Server running on 8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Error starting server.")
+	}
 }
