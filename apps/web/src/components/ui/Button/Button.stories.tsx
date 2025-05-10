a8efb3e6352b208074bb97bd4fb64defca213c60
Alexander Wang <alexander.yisu.wang@outlook.com>
2025-05-10 13:43:20 -0500

    feat: add storybook and fixed docker bug

---
+++ apps/web/src/components/ui/Button/Button.stories.tsx
@@ -0,0 +1,48 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+import { Button } from "./Button";
+
+export const ActionsData = {
+  onClick: fn(),
+};
+
+const meta = {
+  component: Button,
+  title: "UI/Button",
+  tags: ["autodocs"],
+  args: {
+    ...ActionsData,
+    color: "primary",
+    children: "Button",
+  },
+  argTypes: {
+    color: {
+      options: ["primary", "secondary", "danger"],
+      control: { type: "select" },
+    },
+  },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    color: "primary",
+    children: "Primary Button",
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    color: "secondary",
+    children: "Secondary Button",
+  },
+};
+
+export const Danger: Story = {
+  args: {
+    color: "danger",
+    children: "Danger Button",
+  },
+};
