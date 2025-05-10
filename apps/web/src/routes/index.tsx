ecd44cf542b4778849c584355051ee9e1f765a84
Alexander Wang <98280966+AlexanderWangY@users.noreply.github.com>
2025-05-10 13:47:01 -0500

    feat: add storybook and fix docker bug (#11)
    * feat: add storybook and fixed docker bug
    
    * chore: prettier formatting

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,5 +1,5 @@
+import { Button } from "@/components/ui/Button/Button";
 import { createFileRoute } from "@tanstack/react-router";
-import { Button } from "@/components/button";
 
 export const Route = createFileRoute("/")({
   component: Index,
@@ -9,11 +9,8 @@ function Index() {
   return (
     <div className="p-2">
       <h3>SwampHacks Portal</h3>
-      <Button
-        onClick={() => console.log("Button clicked!")}
-        className="bg-blue-300 rounded-md p-2 cursor-pointer hover:bg-blue-400"
-      >
-        Click me!
+      <Button color="primary" onPress={() => console.log("Clicked")}>
+        Hello everyone!
       </Button>
     </div>
   );
