cd322e2f9ac7acd9ab08f776bfe3ace818616a03
hieu <hieunguyen.0527@gmail.com>
2025-05-14 13:29:51 -0500

    feat: added frontend oauth

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/Button/Button";
 import { createFileRoute } from "@tanstack/react-router";
+import auth from "@/lib/auth";
 
 export const Route = createFileRoute("/")({
   component: Index,
@@ -9,7 +10,10 @@ function Index() {
   return (
     <div className="p-2">
       <h3>SwampHacks Portal</h3>
-      <Button color="primary" onPress={() => console.log("Clicked")}>
+      <Button
+        color="primary"
+        onPress={() => auth.signInWithOauth({ provider: "discord" })}
+      >
         Hello everyone!
       </Button>
     </div>
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -7,6 +7,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  envPrefix: "SWAMPHACKS",
   plugins: [
     TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
     react(),
