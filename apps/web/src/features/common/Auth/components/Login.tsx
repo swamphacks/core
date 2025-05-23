1eafa48033a6f83c92ca365e189ca6252562045b
hieu <hieunguyen.0527@gmail.com>
2025-05-23 00:57:22 -0500

    feat: added ThemeProvider & theme toggle, global Figtree font, and new features folders for roles

--- apps/web/src/features/common/Auth/components/Login.tsx
+++ apps/web/src/features/common/Auth/components/Login.tsx
@@ -1,11 +1,23 @@
 import { DiscordIcon } from "@/components/icons/Discord";
 import { Button } from "@/components/ui/Button";
+import { useTheme } from "@/components/ThemeProvider";
+
+const Logo = ({ src }: { src: string }) => {
+  return <img className="py-5" src={src} alt="SwampHacks Logo" />;
+};
 
 const Login = () => {
+  const { theme } = useTheme();
+
   return (
     <div className="flex flex-col items-center bg-surface rounded-md px-3 pt-6 shadow-md text-text-main">
-      {/* <img className="py-5" src="/assets/SwampHacks_Logo_Dark.png" /> */}
-      <img className="py-5" src="/assets/SwampHacks_Logo_Light.png" />
+      <Logo
+        src={
+          theme === "dark"
+            ? "/assets/SwampHacks_Logo_Light.png"
+            : "/assets/SwampHacks_Logo_Dark.png"
+        }
+      />
       <h1 className="text-center font-bold text-2xl">SwampHacks Portal</h1>
       <p className="text-sm text-text-secondary mt-2">
         Log in to access the SwampHacks Portal.
