cfc65d4164b2d2e9a1d16b5c4c9ebd57189965ed
Hieu Nguyen <hieunguyen.0527@gmail.com>
2025-06-06 19:57:06 -0500

    feat: add authentication documentation and export createProvider function

--- apps/web/src/lib/auth/providers.ts
+++ apps/web/src/lib/auth/providers.ts
@@ -2,7 +2,7 @@ import type { OauthProvider } from "./types";
 
 // https://www.typescriptlang.org/docs/handbook/release-notes/typescript-5-0.html#const-type-parameters
 // In short, this allows us to infer the narrowest type possible for each provider object without using "as const" on them
-const createProvider = <const T extends OauthProvider>(provider: T) => {
+export const createProvider = <const T extends OauthProvider>(provider: T) => {
   return provider;
 };
 
