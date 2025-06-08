ff04c005cf8eaa33b07b07cd9504b2696cbc35ea
Hugo Liu <98724522+hugoliu-code@users.noreply.github.com>
2025-06-08 12:13:10 -0700

    ADHOC: Added automatic env struct configuration (#22)
    * feat: Added automatic env struct filling
    
    * fix: Split env Split Words into 2

--- apps/api/internal/config/config.go
+++ apps/api/internal/config/config.go
@@ -3,56 +3,38 @@ package config
 import (
 	"os"
 
+	"github.com/caarlos0/env/v11"
 	"github.com/joho/godotenv"
 	"github.com/rs/zerolog/log"
 )
 
 type OAuthConfig struct {
-	ClientID     string
-	ClientSecret string
-	RedirectUI   string
+	ClientID     string `env:"CLIENT_ID"`
+	ClientSecret string `env:"CLIENT_SECRET"`
+	RedirectURI  string `env:"REDIRECT_URI"`
 }
 
 type AuthConfig struct {
-	Discord OAuthConfig
+	Discord OAuthConfig `envPrefix:"DISCORD_"`
 	// Feel free to add more as implementations grow
 }
 
 type Config struct {
-	DatabaseURL string
-	Port        string
+	DatabaseURL string `env:"DATABASE_URL"`
+	Port        string `env:"PORT" envDefault:"8080"`
 
-	Auth AuthConfig
+	Auth AuthConfig `envPrefix:"AUTH_"`
 }
 
 func Load() *Config {
 	loadEnv()
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
+	cfg, err := env.ParseAs[Config]()
+	if err != nil {
+		log.Fatal().Msgf("Failed to parse env: %v", err)
 	}
 
-	dbURL := os.Getenv("DATABASE_URL")
-	if dbURL == "" {
-		log.Fatal().Msg("DATABASE_URL is required in env")
-	}
-
-	discordClientID := os.Getenv("DISCORD_CLIENT_ID")
-	discordClientSecret := os.Getenv("DISCORD_ClIENT_SECRET")
-	discordRedirectURI := os.Getenv("DISCORD_REDIRECT_URI")
-
-	return &Config{
-		DatabaseURL: dbURL,
-		Port:        port,
-		Auth: AuthConfig{
-			Discord: OAuthConfig{
-				ClientID:     discordClientID,
-				ClientSecret: discordClientSecret,
-				RedirectUI:   discordRedirectURI,
-			},
-		},
-	}
+	return &cfg
 }
 
 func loadEnv() {
