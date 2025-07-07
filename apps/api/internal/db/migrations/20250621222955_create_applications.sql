8a16cb91a03b34204be93aef254630d642e6e481
Alexander Wang <alexander.yisu.wang@outlook.com>
2025-07-06 23:47:36 -0400

    feat: add more columns and auto updates + withdrawn status

--- apps/api/internal/db/migrations/20250621222955_create_applications.sql
+++ apps/api/internal/db/migrations/20250621222955_create_applications.sql
@@ -1,25 +1,33 @@
 -- +goose Up
 -- +goose StatementBegin
 
-CREATE TYPE application_status AS ENUM ('open', 'submitted', 'under_review', 'accepted', 'rejected', 'waitlisted');
+CREATE TYPE application_status AS ENUM ('started', 'submitted', 'under_review', 'accepted', 'rejected', 'waitlisted', 'withdrawn');
 
 CREATE TABLE applications (
     user_id UUID REFERENCES auth.users(id) ON DELETE CASCADE,
     event_id UUID REFERENCES events(id) ON DELETE CASCADE,
-    status application_status DEFAULT 'open',
+    status application_status DEFAULT 'started',
     application JSONB,
     resume_url TEXT,
-    created_at TIMESTAMPTZ DEFAULT NOW(),
-    updated_at TIMESTAMPTZ,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    saved_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
 
     PRIMARY KEY (user_id, event_id)
 );
+
+-- Create trigger to update application updates
+CREATE TRIGGER set_updated_at_applications
+BEFORE UPDATE ON applications
+FOR EACH ROW
+EXECUTE FUNCTION update_modified_column();
 
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
 
+DROP TRIGGER IF EXISTS set_updated_at_applications;
 DROP TABLE IF EXISTS applications;
 DROP TYPE IF EXISTS application_status;
 
