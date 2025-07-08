7a8c56a61fdb6a498e761429e0fafa01b577a1be
Alexander Wang <alexander.yisu.wang@outlook.com>
2025-07-08 19:21:14 -0400

    fix: move to pgconn for error handling

--- apps/api/internal/db/errors.go
+++ apps/api/internal/db/errors.go
@@ -3,15 +3,13 @@ package db
 import (
 	"errors"
 
-	"github.com/lib/pq"
+	"github.com/jackc/pgx/v5/pgconn"
 )
 
 func IsUniqueViolation(err error) bool {
-	var pqErr *pq.Error
-
-	if errors.As(err, &pqErr) {
-		return pqErr.Code == "23505"
+	var pgErr *pgconn.PgError
+	if errors.As(err, &pgErr) {
+		return pgErr.Code == "23505"
 	}
-
 	return false
 }
