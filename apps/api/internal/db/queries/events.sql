3c4417c73a7bf330d7213e696ac313e4e69d9de2
h1divp <71522316+h1divp@users.noreply.github.com>
2025-07-11 18:44:05 -0400

    improvement: improved even queries

--- apps/api/internal/db/queries/events.sql
+++ apps/api/internal/db/queries/events.sql
@@ -1,7 +1,7 @@
 -- name: CreateEvent :one
 INSERT INTO events (
     id, name, description,
-    location, loction_url, max_attendees,
+    location, location_url, max_attendees,
     application_open, application_close, rsvp_deadline, decision_release,
     start_time, end_time,
     website_url 
@@ -14,79 +14,33 @@ INSERT INTO events (
 )
 RETURNING *;
 
--- name: GetEventByID: one
+-- name: GetEventByID :one
 SELECT * FROM events
 WHERE id = $1;
 
--- name: GetEventByLocation: many
+-- name: GetEventByLocation :many
 SELECT * FROM events
 WHERE location = $1;
 
--- name: UpdateEventName: exec
+-- name: UpdateEvent :exec
 UPDATE events
-SET name = $2
-WHERE id = $1;
-
--- name: UpdateEventDescription: exec
-UPDATE events
-SET description = $2
-WHERE id = $1;
-
--- name: UpdateEventLocation: exec
-UPDATE events
-SET location = $2
-WHERE id = $1;
-
--- name: UpdateEventLocationUrl: exec
-UPDATE events
-SET location_url = $2
-WHERE id = $1;
-
--- name: UpdateEventMaxAttendees: exec
-UPDATE events
-SET max_attendees = $2
-WHERE id = $1;
-
--- name: UpdateEventApplicationOpen: exec
-UPDATE events
-SET application_open = $2
-WHERE id = $1;
-
--- name: UpdateEventApplicationClose: exec
-UPDATE events
-SET application_close = $2
-WHERE id = $1;
-
--- name: UpdateEventRsvpDeadline: exec
-UPDATE events
-SET rsvp_deadline = $2
-WHERE id = $1;
-
--- name: UpdateEventDecisionRelease: exec
-UPDATE events
-SET decision_release = $2
-WHERE id = $1;
-
--- name: UpdateEventStartTime: exec
-UPDATE events
-SET start_time = $2
-WHERE id = $1;
-
--- name: UpdateEventEndTime: exec
-UPDATE events
-SET end_time = $2
-WHERE id = $1;
-
--- name: UpdateEventIsPublished: exec
-UPDATE events
-SET is_published = $2
-WHERE id = $1;
-
--- name: UpdateEventWebsiteUrl: exec
-UPDATE events
-SET website_url = $2
-WHERE id = $1;
- 
--- name: DeleteEvent: exec
+SET
+    name = CASE WHEN @name_do_update::boolean THEN @name ELSE name END,
+    description = CASE WHEN @description_do_update::boolean THEN @description ELSE description END,
+    location = CASE WHEN @location_do_update::boolean THEN @location ELSE location END,
+    location_url = CASE WHEN @location_url_do_update::boolean THEN @location_url ELSE location_url END,
+    max_attendees = CASE WHEN @max_attendees_do_update::boolean THEN @max_attendees ELSE max_attendees END,
+    application_open = CASE WHEN @application_open_do_update::boolean THEN @application_open ELSE application_open END,
+    application_close = CASE WHEN @application_close_do_update::boolean THEN @application_close ELSE application_close END,
+    rsvp_deadline = CASE WHEN @rsvp_deadline_do_update::boolean THEN @rsvp_deadline ELSE rsvp_deadline END,
+    decision_release = CASE WHEN @decision_release_do_update::boolean THEN @decision_release ELSE decision_release END,
+    start_time = CASE WHEN @start_time_do_update::boolean THEN @start_time ELSE start_time END,
+    end_time = CASE WHEN @end_time_do_update::boolean THEN @end_time ELSE end_time END,
+    website_url = CASE WHEN @website_url_do_update::boolean THEN @website_url ELSE website_url END,
+    is_published = CASE WHEN @is_published_do_update::boolean THEN @is_published ELSE is_published END
+WHERE
+    id = @id::uuid;
+    
+-- name: DeleteEvent :exec
 DELETE FROM events
 WHERE id = $1;
