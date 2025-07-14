41d4c2c5beddaf454586bc5e9e489a2723f890b9
Hieu Nguyen <76720778+hieunguyent12@users.noreply.github.com>
2025-07-14 08:08:21 -0500

    feat: added event card (#58)
    * feat: added event card
    
    * fix: full round + more padding
    
    * fix: updated buttons for accepted status and event details links
    
    * fix: used Link from react-aria-component instead
    
    ---------
    
    Co-authored-by: Alexander Wang <alexander.yisu.wang@outlook.com>

---
+++ apps/web/src/features/Event/components/stories/EventCard.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { EventCard as EventCardComp } from "../EventCard";
+import applicationStatus from "../../applicationStatus";
+
+const meta = {
+  component: EventCardComp,
+  title: "UI/Event Card",
+  tags: ["autodocs"],
+  argTypes: {
+    status: {
+      control: {
+        type: "select",
+      },
+      options: Object.keys(applicationStatus),
+    },
+  },
+} satisfies Meta<typeof EventCardComp>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const EventCard: Story = {
+  args: {
+    eventId: "test id",
+    status: "rejected",
+    title: "SwampHacks XI",
+    description: "UF’s flagship hackathon celebrates its 11th iteration.",
+    date: "Jan 25 - 26",
+    location: "Newell Hall",
+  },
+};
