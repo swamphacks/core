a84c3b0841dcfdb186bf4f47f6c0a301b5cbc626
Alexander Wang <98280966+AlexanderWangY@users.noreply.github.com>
2025-07-19 14:24:48 -0400

    Admin dashboard links (#69)
    * feat: added admin links
    
    * fix: make the link only appear on superuser status

--- apps/web/src/routes/_main/resources/programming.tsx
+++ apps/web/src/routes/_main/resources/programming.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute("/_protected/resources/programming")({
+export const Route = createFileRoute("/_main/resources/programming")({
   component: RouteComponent,
 });
 
