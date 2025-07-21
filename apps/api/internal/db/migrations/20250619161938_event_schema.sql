975dc0efcf715ef46cd42398d1662be4ab8467ec
Phoenix <71522316+h1divp@users.noreply.github.com>
2025-07-21 16:39:34 -0400

    Revert "TECH-123: Create sql query to fetch event, user, and application information"

--- apps/api/internal/db/migrations/20250619161938_event_schema.sql
+++ apps/api/internal/db/migrations/20250619161938_event_schema.sql
@@ -13,6 +13,7 @@ CREATE TABLE events (
     application_close TIMESTAMPTZ NOT NULL,
     rsvp_deadline TIMESTAMPTZ,
     decision_release TIMESTAMPTZ,
+    
 
     -- Event phase
     start_time TIMESTAMPTZ NOT NULL,
@@ -21,7 +22,6 @@ CREATE TABLE events (
     -- Metadata
     website_url TEXT,
     is_published BOOLEAN DEFAULT FALSE,
-    saved_at TIMESTAMPTZ DEFAULT NOW(),
 
     created_at TIMESTAMPTZ DEFAULT NOW(),
     updated_at TIMESTAMPTZ DEFAULT NOW()
