04e63d2a2a4928f4d1d740801752f437f5dd24cc
Hieu Nguyen <hieunguyen.0527@gmail.com>
2025-07-23 21:35:16 -0500

    chore: disable eslint rule for now, removed application folder and small tweaks

--- apps/web/src/components/Form/Form.stories.tsx
+++ apps/web/src/components/Form/Form.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta } from "@storybook/react";
 import { Form, useAppForm } from ".";
 
 const meta: Meta<typeof Form> = {
-  title: "UI/Form",
+  title: "Form/Example",
   component: Form,
   parameters: {
     layout: "centered",
@@ -20,45 +20,59 @@ export const Example = () => {
   });
 
   return (
-    <Form
-      onSubmit={(e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        form.handleSubmit();
-      }}
-    >
-      <form.AppField
-        name="email"
-        children={(field) => (
-          <field.TextField
-            label="Email"
-            name={field.name}
-            type="email"
-            autoComplete="off"
-            className="flex-1"
-            placeholder="Enter your email"
-            isRequired={true}
-          />
-        )}
-      />
+    <div className="bg-surface p-10 max-w-100">
+      <Form
+        onSubmit={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          form.handleSubmit();
+        }}
+      >
+        <form.AppField
+          name="email"
+          children={(field) => (
+            <field.TextField
+              label="Email"
+              name={field.name}
+              type="email"
+              autoComplete="off"
+              className="flex-1"
+              placeholder="Enter your email"
+              isRequired={true}
+            />
+          )}
+        />
 
-      <form.AppField
-        name="date"
-        children={(field) => (
-          <field.DatePickerField
-            label="Date"
-            name={field.name}
-            className="flex-1"
-            isRequired={true}
-          />
-        )}
-      />
+        <form.AppField
+          name="date"
+          children={(field) => (
+            <field.DatePickerField
+              label="Date"
+              name={field.name}
+              className="flex-1"
+              isRequired={true}
+            />
+          )}
+        />
 
-      <div className="flex gap-2">
-        <form.AppForm>
-          <form.SubmitButton label="Submit" />
-        </form.AppForm>
-      </div>
-    </Form>
+        <form.AppField
+          name="upload"
+          children={(field) => (
+            <field.UploadField
+              label="Upload"
+              // className="flex-1"
+              isRequired={true}
+              description="Allowed file types: .pdf, .docx"
+            />
+          )}
+        />
+
+        <div className="flex gap-2">
+          <form.AppForm>
+            <form.SubmitButton label="Submit" />
+          </form.AppForm>
+        </div>
+      </Form>
+    </div>
   );
 };
