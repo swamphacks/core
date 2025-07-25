e87a9b7b8306aca1916801b74bf5fdaced0d4822
Hieu Nguyen <hieunguyen.0527@gmail.com>
2025-07-25 13:49:42 -0500

    chore: updated application form example

--- apps/web/src/features/FormBuilder/stories/example.stories.tsx
+++ apps/web/src/features/FormBuilder/stories/example.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta } from "@storybook/react";
 import { build } from "../build";
 import data from "./example.json";
+import applicationFormData from "./applicationFormExample.json";
 
 const meta: Meta = {
   title: "Form Builder/Example",
@@ -15,4 +16,18 @@ export default meta;
 export const Test = () => {
   const Form = build(data);
   return <Form />;
+};
+
+export const ApplicationForm = () => {
+  const Form = build(applicationFormData);
+  return (
+    <div className="w-full h-screen bg-surface">
+      <div className="w-full bg-surface transition-[background]">
+        <Form />
+      </div>
+    </div>
+  );
+};
+ApplicationForm.parameters = {
+  layout: "fullscreen",
 };
