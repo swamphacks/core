d8908c596645bb22a06215b8b6039c806258cc0c
Alexander Wang <98280966+AlexanderWangY@users.noreply.github.com>
2025-10-06 22:35:59 -0400

    Change API call to fetch all events

--- apps/web/src/features/PlatformAdmin/EventManager/hooks/useAdminEvents.ts
+++ apps/web/src/features/PlatformAdmin/EventManager/hooks/useAdminEvents.ts
@@ -9,7 +9,7 @@ type Events =
 
 export async function fetchEvents(): Promise<Events> {
   const result = await api
-    .get<Events>("events?include_unpublished=true")
+    .get<Events>("events?scope=all")
     .json();
   console.log(result);
   return result;
