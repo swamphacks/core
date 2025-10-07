b768a1174c7cc8cc174d023623584df99006a258
Alexander Wang <98280966+AlexanderWangY@users.noreply.github.com>
2025-10-07 18:02:12 -0400

    Refactor event routes to simplify authentication
    Removed redundant authentication middleware for interest route.

--- apps/api/internal/api/api.go
+++ apps/api/internal/api/api.go
@@ -121,11 +121,7 @@ func (api *API) setupRoutes(mw *mw.Middleware) {
 		// Event-specific routes
 		r.Route("/{eventId}", func(r chi.Router) {
 			r.Use(mw.Auth.RequireAuth) // routes below this are protected
-
-			r.Post("/interest", api.Handlers.EventInterest.AddEmailToEvent)
-
-			// General access
-			r.Use(mw.Auth.RequireAuth) // routes below this are protected
+			
 			r.Get("/", api.Handlers.Event.GetEventByID)
 			r.Get("/role", api.Handlers.Event.GetEventRole)
 
