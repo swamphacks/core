8c6538bb6faf846d4eef48b4dfb31dee84750082
Hieu Nguyen <hieunguyen.0527@gmail.com>
2025-10-10 00:32:17 -0400

    chore: update application forms and some small improvements

--- apps/web/src/components/Form/Form.tsx
+++ apps/web/src/components/Form/Form.tsx
@@ -1,16 +1,18 @@
 import { type FormProps, Form as RACForm } from "react-aria-components";
 import { cn } from "@/utils/cn";
-import { Suspense, type PropsWithChildren } from "react";
+import { memo, Suspense, type PropsWithChildren } from "react";
 import { Spinner } from "@/components/ui/Spinner";
 
-export function Form(props: FormProps & React.RefAttributes<HTMLFormElement>) {
+export const Form = memo(function Form(
+  props: FormProps & React.RefAttributes<HTMLFormElement>,
+) {
   return (
     // We use suspense here since form fields are lazy loaded
     <FormSuspense>
       <RACForm {...props} className={cn("", props.className)} />
     </FormSuspense>
   );
-}
+});
 
 export function FormSuspense({ children }: PropsWithChildren) {
   return (
