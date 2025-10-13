1c91c0328c88225426c57394113a8e02c8db6802
h1divp <71522316+h1divp@users.noreply.github.com>
2025-10-13 16:50:09 -0400

    fix: parsing NOW uses correct path

--- apps/api/internal/services/email.go
+++ apps/api/internal/services/email.go
@@ -85,7 +85,8 @@ func (s *EmailService) SendConfirmationEmail(recipient string, name string) erro
 
 	var body bytes.Buffer
 
-	template, err := template.ParseFiles("../../internal/email/templates/ConfirmationEmail.html")
+	// file path changes to "../../internal/email/templates/ConfirmationEmail.html" if you're testing via web api
+	template, err := template.ParseFiles("./internal/email/templates/ConfirmationEmail.html")
 	if err != nil {
 		s.logger.Err(err).Msg("Failed to parse email template for recipient")
 	}
