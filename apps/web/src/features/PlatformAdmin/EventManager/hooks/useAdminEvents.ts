e138b4d03a55a531c20db9fcc76e28c8360bd707
Alexander Wang <alexander.yisu.wang@outlook.com>
2025-10-16 16:12:54 -0400

    format: random formatting fix

--- apps/web/src/features/PlatformAdmin/EventManager/hooks/useAdminEvents.ts
+++ apps/web/src/features/PlatformAdmin/EventManager/hooks/useAdminEvents.ts
@@ -8,9 +8,7 @@ type Events =
   paths["/events"]["get"]["responses"]["200"]["content"]["application/json"];
 
 export async function fetchEvents(): Promise<Events> {
-  const result = await api
-    .get<Events>("events?scope=all")
-    .json();
+  const result = await api.get<Events>("events?scope=all").json();
   console.log(result);
   return result;
 }
