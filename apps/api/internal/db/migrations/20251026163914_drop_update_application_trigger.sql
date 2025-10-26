b8be35a72bed6c75b1cbae86d92d254b1fc17ff3
h1divp <71522316+h1divp@users.noreply.github.com>
2025-10-26 12:47:54 -0400

    fix: trigger and function dropped

--- apps/api/internal/db/migrations/20251026163914_drop_update_application_trigger.sql
+++ apps/api/internal/db/migrations/20251026163914_drop_update_application_trigger.sql
@@ -1,10 +1,6 @@
 -- +goose Up
 -- +goose StatementBegin
 DROP TRIGGER IF EXISTS set_updated_at_applications ON applications;
-
-CREATE TRIGGER set_updated_at_applications
-BEFORE UPDATE ON applications
-FOR EACH ROW
-EXECUTE FUNCTION update_modified_column();
+DROP FUNCTION IF EXISTS update_application_modified_column;
 
 -- +goose StatementEnd
