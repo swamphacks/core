d0718a7770d1beebfbf302d10955f12fbf3ef717
h1divp <71522316+h1divp@users.noreply.github.com>
2025-10-28 15:04:44 -0400

    Added Goose Down migration

--- apps/api/internal/db/migrations/20251026163914_drop_update_application_trigger.sql
+++ apps/api/internal/db/migrations/20251026163914_drop_update_application_trigger.sql
@@ -2,5 +2,10 @@
 -- +goose StatementBegin
 DROP TRIGGER IF EXISTS set_updated_at_applications ON applications;
 DROP FUNCTION IF EXISTS update_application_modified_column;
+-- +goose StatementEnd
 
+-- +goose Down
+-- +goose StatementBegin
+DROP TRIGGER IF EXISTS set_updated_at_applications ON applications;
+DROP FUNCTION IF EXISTS update_application_modified_column;
 -- +goose StatementEnd
