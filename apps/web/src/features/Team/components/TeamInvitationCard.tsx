c651471a21e5e9cc2dc935af146581fd3c1a0a7e
Alexander Wang <alexander.yisu.wang@outlook.com>
2025-11-08 12:17:03 -0600

    chore: select correct icon for button

--- apps/web/src/features/Team/components/TeamInvitationCard.tsx
+++ apps/web/src/features/Team/components/TeamInvitationCard.tsx
@@ -1,5 +1,7 @@
 import { Button } from "@/components/ui/Button";
+import { OverlayArrow, Tooltip, TooltipTrigger } from "react-aria-components";
 import TablerCheck from "~icons/tabler/check";
+import TablerCircleX from "~icons/tabler/circle-x";
 
 export default function TeamInvitationCard() {
   return (
@@ -9,7 +11,19 @@ export default function TeamInvitationCard() {
         <strong>The Pirates</strong>.
       </p>
       <div className="flex flex-row">
-        <Button variant="icon">X</Button>
+        <TooltipTrigger delay={150}>
+          <Button variant="icon" className="aspect-square p-2.5">
+            <TablerCircleX className="h-5 w-5" />
+          </Button>
+          <Tooltip>
+            <OverlayArrow>
+              <svg width={8} height={8} viewBox="0 0 8 8">
+                <path d="M0 0 L4 4 L8 0" />
+              </svg>
+            </OverlayArrow>
+            Reject
+          </Tooltip>
+        </TooltipTrigger>
         <Button variant="icon" className="aspect-square p-2.5">
           <TablerCheck className="h-5 w-5" />
         </Button>
