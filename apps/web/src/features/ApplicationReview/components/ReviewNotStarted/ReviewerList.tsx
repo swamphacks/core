4236e4b816532d50e068ecc450e1ac5fa0041d53
AlexanderWangY <alexander.yisu.wang@outlook.com>
2025-11-21 13:43:01 -0500

    chore: refactor into components

---
+++ apps/web/src/features/ApplicationReview/components/ReviewNotStarted/ReviewerList.tsx
@@ -0,0 +1,87 @@
+import { Checkbox, CheckboxGroup } from "@/components/ui/Checkbox";
+import { NumberField, Input } from "react-aria-components";
+import { Tooltip } from "@/components/ui/Tooltip";
+import TablerHelpCircle from "~icons/tabler/help-circle";
+import type { StaffUsers } from "@/features/PlatformAdmin/EventManager/hooks/useEventStaffUsers";
+import type { AssignedReviewer } from "./ReviewerAssignmentModal";
+import type { Dispatch, SetStateAction } from "react";
+import { Button } from "@/components/ui/Button";
+
+interface Props {
+  staff: StaffUsers;
+  assigned: AssignedReviewer[];
+  setAssigned: Dispatch<SetStateAction<AssignedReviewer[]>>;
+}
+
+export default function ReviewerList({ staff, assigned, setAssigned }: Props) {
+  const handleSelectionChange = (selected: string[]) => {
+    const updated = selected.map((id) => {
+      const existing = assigned.find((r) => r.id === id);
+      return existing ?? { id, amount: null };
+    });
+
+    setAssigned(updated);
+  };
+
+  return (
+    <>
+      <div className="flex justify-between text-sm text-text-secondary mt-2">
+        <p>Reviewer</p>
+
+        <div className="flex gap-1 items-center">
+          <Tooltip
+            tooltipProps={{
+              label:
+                "If blank, remaining applications will be auto-distributed.",
+            }}
+            triggerProps={{
+              delay: 200,
+            }}
+          >
+            <Button variant="unstyled" className="p-0 m-0">
+              <TablerHelpCircle className="h-4 w-4" />
+            </Button>
+          </Tooltip>
+          <p># of Apps</p>
+        </div>
+      </div>
+
+      <CheckboxGroup onChange={handleSelectionChange}>
+        {staff.map((s) => {
+          const isSelected = assigned.some((a) => a.id === s.id);
+
+          return (
+            <div className="flex justify-between w-full" key={s.id}>
+              <Checkbox value={s.id} className="flex-1">
+                <div className="flex flex-row items-center gap-2">
+                  <span>{s.name || "Anonymous"}</span>
+                  <span className="text-xs text-text-secondary">{s.email}</span>
+                </div>
+              </Checkbox>
+
+              <NumberField className="ml-auto">
+                <Input
+                  disabled={!isSelected}
+                  onChange={(e) => {
+                    const value = e.target.value;
+                    setAssigned((prev) =>
+                      prev.map((r) =>
+                        r.id === s.id
+                          ? {
+                              ...r,
+                              amount: value === "" ? null : Number(value),
+                            }
+                          : r,
+                      ),
+                    );
+                  }}
+                  className="border-border border-2 w-12 h-6 text-sm px-1 disabled:bg-input-bg-disbaled dark:disabled:bg-neutral-800 disabled:cursor-not-allowed"
+                />
+              </NumberField>
+            </div>
+          );
+        })}
+      </CheckboxGroup>
+    </>
+  );
+}
--- apps/web/src/routes/_protected/events/$eventId/dashboard/_staff/application-review.tsx
+++ apps/web/src/routes/_protected/events/$eventId/dashboard/_staff/application-review.tsx
@@ -1,18 +1,9 @@
-import { Button } from "@/components/ui/Button";
-import { Checkbox, CheckboxGroup } from "@/components/ui/Checkbox";
-import { Modal } from "@/components/ui/Modal";
-import { Tooltip } from "@/components/ui/Tooltip";
+import { useApplicationStatistics } from "@/features/Application/hooks/useApplicationStatistics";
+import ReviewNotStarted from "@/features/ApplicationReview/components/ReviewNotStarted/ReviewNotStarted";
+import { useEvent } from "@/features/Event/hooks/useEvent";
 import { useEventStaffUsers } from "@/features/PlatformAdmin/EventManager/hooks/useEventStaffUsers";
 import { createFileRoute } from "@tanstack/react-router";
-import { useState } from "react";
-import {
-  DialogTrigger,
-  Heading,
-  Input,
-  NumberField,
-  Separator,
-} from "react-aria-components";
-import TablerHelpCircle from "~icons/tabler/help-circle";
+import { Heading } from "react-aria-components";
 
 export const Route = createFileRoute(
   "/_protected/events/$eventId/dashboard/_staff/application-review",
@@ -20,71 +11,30 @@ export const Route = createFileRoute(
   component: RouteComponent,
 });
 
-interface AssignedReviewer {
-  id: string;
-  amount: number | null;
-}
-
 function RouteComponent() {
-  const { user, eventRole } = Route.useRouteContext();
   const { eventId } = Route.useParams();
-  const eventStaff = useEventStaffUsers(eventId);
+  const { user, eventRole } = Route.useRouteContext();
 
-  const [assignedReviewer, setAssignedReviewer] = useState<AssignedReviewer[]>(
-    [],
-  );
+  const event = useEvent(eventId);
+  const stats = useApplicationStatistics(eventId);
+  const staff = useEventStaffUsers(eventId);
 
-  const numOfApplicationsAssigned = assignedReviewer.reduce((acc, curr) => {
-    return acc + (curr.amount ?? 0);
-  }, 0);
-  const mockNumOfApplications = 1000;
-  const remainingApplications =
-    mockNumOfApplications - numOfApplicationsAssigned;
+  const loading =
+    !user ||
+    event.isLoading ||
+    stats.isLoading ||
+    staff.isLoading ||
+    !event.data ||
+    !stats.data ||
+    !staff.data;
 
-  const onHandleSelectionChange = (selected: string[]) => {
-    // Each string in selected is a user ID
-    // Update the assignedReviewer state accordingly
-
-    const updatedAssignedReviewers = selected.map((id) => {
-      const existing = assignedReviewer.find((rev) => rev.id === id);
-      return existing ? existing : { id, amount: null };
-    });
-
-    setAssignedReviewer(updatedAssignedReviewers);
-  };
-
-  const handleSubmit = () => {
-    const hasNullAmount = assignedReviewer.some((rev) => rev.amount === null);
-    if (remainingApplications < 0) {
-      alert("Assigned applications exceed total number of applications.");
-      return;
-    }
-
-    if (remainingApplications > 0 && !hasNullAmount) {
-      // Check if there are any reviewers with null amount
-      alert(
-        "Please leave at least one reviewer with no specified amount to distribute remaining applications.",
-      );
-      return;
-    }
-
-    if (assignedReviewer.length === 0) {
-      alert("R U DEADAHH RN?");
-      return;
-    }
-
-    console.log("Assigned Reviewers:", assignedReviewer);
-  };
-
-  if (!user || eventStaff.isLoading) {
+  if (loading) {
     return (
       <main>
         <Heading className="text-2xl lg:text-3xl font-semibold mb-6">
-          My Team
+          Application Review
         </Heading>
-        <div className="flex flex-col gap-4 max-w-xl">
-          <div className="h-84 w-full md:w-120 bg-neutral-300 dark:bg-neutral-800 rounded animate-pulse"></div>
-        </div>
+        <div className="h-84 w-full max-w-xl bg-neutral-300 dark:bg-neutral-800 rounded animate-pulse" />
       </main>
     );
   }
@@ -95,111 +45,18 @@ function RouteComponent() {
         Application Review
       </Heading>
 
-      {eventRole === "staff" ? (
-        <div>
-          <p className="text-text-secondary">
-            Application review has not started yet. Check back later!
-          </p>
-        </div>
-      ) : (
-        <div className="flex flex-col gap-4">
-          <p className="text-text-secondary">
-            Application review has not started.
-          </p>
-          <DialogTrigger
-            onOpenChange={(open) => !open && setAssignedReviewer([])}
-          >
-            <Button variant="primary" className="w-fit">
-              Start Now
-            </Button>
+      {eventRole === "staff" && (
+        <p className="text-text-secondary">
+          Application review has not started yet. Come back later!
+        </p>
+      )}
 
-            <Modal>
-              <div className="flex flex-col">
-                <h2>Select Reviewers</h2>
-                <div className="flex flex-row w-full items-center justify-between text-text-secondary text-sm mt-2">
-                  <p>Reviewer</p>
-
-                  <div className="flex flex-row gap-1 items-center justify-center">
-                    <Tooltip
-                      tooltipProps={{
-                        label:
-                          "Number of applications to assign to this reviewer. If left blank, we will automatically distribute remaining applications among all blank selected reviewers.",
-                      }}
-                      triggerProps={{
-                        delay: 200,
-                      }}
-                    >
-                      <Button variant="unstyled" className="p-0 m-0">
-                        <TablerHelpCircle className="inline-block h-4 w-4 mt-0.5" />
-                      </Button>
-                    </Tooltip>
-
-                    <p># of Apps</p>
-                  </div>
-                </div>
-                <CheckboxGroup onChange={onHandleSelectionChange}>
-                  {eventStaff.data?.map((staffUser) => (
-                    <div
-                      className="flex flex-row items-center justify-between w-full"
-                      key={staffUser.id}
-                    >
-                      <Checkbox value={staffUser.id} className="flex-1">
-                        <div className="flex flex-row items-center gap-2">
-                          <span>{staffUser.name || "Anonymous"}</span>
-                          <span className="text-text-secondary text-xs">
-                            {staffUser.email}
-                          </span>
-                        </div>
-                      </Checkbox>
-
-                      <NumberField className="ml-auto">
-                        <Input
-                          onChange={(e) => {
-                            const value = e.target.value;
-                            setAssignedReviewer((prev) =>
-                              prev.map((rev) =>
-                                rev.id === staffUser.id
-                                  ? {
-                                      ...rev,
-                                      amount:
-                                        value === "" ? null : Number(value),
-                                    }
-                                  : rev,
-                              ),
-                            );
-                          }}
-                          className="border-border border-2 w-12 h-6 text-sm disabled:bg-neutral-200 dark:disabled:bg-neutral-800 disabled:hover:cursor-not-allowed px-1"
-                          disabled={
-                            !assignedReviewer.find(
-                              (rev) => rev.id === staffUser.id,
-                            )
-                          }
-                        />
-                      </NumberField>
-                    </div>
-                  ))}
-                </CheckboxGroup>
-
-                <Separator className="my-4 text-neutral-700" />
-
-                <div className="flex flex-col items-end text-text-secondary text-sm">
-                  <p>Assigned Applications: {numOfApplicationsAssigned}</p>
-                  <p>Total Applications: {mockNumOfApplications}</p>
-                  <p>Remaining: {remainingApplications}</p>
-                </div>
-
-                <div className="mt-4 flex justify-end gap-2">
-                  <Button slot="close" variant="secondary">
-                    Cancel
-                  </Button>
-                  <Button variant="primary" onClick={handleSubmit}>
-                    Assign Reviewers
-                  </Button>
-                </div>
-              </div>
-            </Modal>
-          </DialogTrigger>
-        </div>
+      {eventRole === "admin" && (
+        <ReviewNotStarted
+          event={event.data}
+          stats={stats.data}
+          staff={staff.data}
+        />
       )}
     </main>
   );
