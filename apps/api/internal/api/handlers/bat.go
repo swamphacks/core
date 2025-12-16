eeae581e65bee91a35ac03b7dbdd48f880c862b9
h1divp <71522316+h1divp@users.noreply.github.com>
2025-12-16 17:26:24 -0500

    feat: update review finished status handler

--- apps/api/internal/api/handlers/bat.go
+++ apps/api/internal/api/handlers/bat.go
@@ -69,6 +69,53 @@ func (h *BatHandler) GetRunsByEventId(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// Update event application_reviews_finished status
+//
+//	@Summary		Update an event's application_reviews_finished status
+//	@Description	Update an event's application_reviews_finished status
+//	@Tags			Event
+//	@Accept			json
+//	@Produce		json
+//	@Param			eventId	path	string	true	"Event ID"	Format(uuid)
+//	@Success		204		"OK - Event updated (patched)"
+//	@Failure		500		{object}	response.ErrorResponse	"Server Error: Something went terribly wrong on our end."
+//	@Router			/events/{eventId} [patch]
+func (h *BatHandler) UpdateEventApplicationReviewsFinishedStatus(w http.ResponseWriter, r *http.Request) {
+	eventIdStr := chi.URLParam(r, "eventId")
+	if eventIdStr == "" {
+		res.SendError(w, http.StatusBadRequest, res.NewError("missing_event_id", "The event ID is missing from the URL!"))
+		return
+	}
+	eventId, err := uuid.Parse(eventIdStr)
+
+	if err != nil {
+		res.SendError(w, http.StatusBadRequest, res.NewError("invalid_event_id", "The event ID is not a valid UUID"))
+		return
+	}
+
+	reviewsNotFinished, err := h.BatService.UpdateEventApplicationReviewsFinishedStatus(r.Context(), eventId)
+	if errors.Is(err, services.ErrMissingFields) {
+		res.SendError(w, http.StatusBadRequest, res.NewError("missing_fields", "Missing/malformed query. Available parameters:  eventId"))
+		return
+	}
+
+	if errors.Is(err, services.ErrMissingPerms) {
+		res.SendError(w, http.StatusForbidden, res.NewError("forbidden", "You are forbidden from this resource."))
+		return
+	}
+
+	if err != nil {
+		res.SendError(w, http.StatusInternalServerError, res.NewError("internal_err", "Something went wrong encoding response"))
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(reviewsNotFinished); err != nil {
+		res.SendError(w, http.StatusInternalServerError, res.NewError("internal_err", "Something went wrong encoding response"))
+		return
+	}
+}
+
 // Delete an event
 //
 //	@Summary		Delete an event
