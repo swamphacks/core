db411a74447095783e092bdba6325ec60e2dbb52
h1divp <71522316+h1divp@users.noreply.github.com>
2026-01-10 19:49:45 -0500

    fix: set period to 72hr; chore: removed orphaned code

--- apps/api/internal/services/bat.go
+++ apps/api/internal/services/bat.go
@@ -362,9 +362,10 @@ func (s *BatService) mapToCandidates(engine *bat.BatEngine, applications []sqlc.
 
 func (s *BatService) QueueScheduleWaitlistTransitionTask(ctx context.Context, eventId uuid.UUID) error {
 
+	// TODO: add period to config?
 	task, err := tasks.NewTaskScheduleTransitionWaitlist(tasks.ScheduleTransitionWaitlistPayload{
 		EventID: eventId,
-		Period:  "@every 10s",
+		Period:  "@every 72h",
 	})
 	if err != nil {
 		s.logger.Err(err).Msg("Failed to create ScheduleTransitionWaitlist task")
