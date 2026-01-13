b5edbc1e06da602587b9d699a205e2a8aa0c0887
Hugo Liu <98724522+hugoliu-code@users.noreply.github.com>
2026-01-12 22:26:12 -0500

    Frontend

--- apps/web/src/features/EventOverview/components/AttendeeOverview.tsx
+++ apps/web/src/features/EventOverview/components/AttendeeOverview.tsx
@@ -1,3 +1,26 @@
-export default function AttendeeOverview() {
-  return <div>More here coming soon!</div>;
+import { DialogTrigger } from "react-aria-components";
+import { EventAttendanceWithdrawalModal } from "@/features/Event/components/EventAttendanceWithdrawalModal";
+import { Button } from "@/components/ui/Button";
+
+interface ApplicationOverviewProps {
+  eventId: string;
+}
+
+export default function AttendeeOverview({
+  eventId,
+}: ApplicationOverviewProps) {
+  return (
+    <div>
+      <div>More here coming soon!</div>
+      <div>
+        <p className="my-5">Can't make it to the event?</p>
+        <DialogTrigger>
+          <Button variant="danger">{"Withdraw Attendance"}</Button>
+          <EventAttendanceWithdrawalModal
+            eventId={eventId}
+          ></EventAttendanceWithdrawalModal>
+        </DialogTrigger>
+      </div>
+    </div>
+  );
 }
