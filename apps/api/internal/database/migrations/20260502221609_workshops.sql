a388c09fe5cee69305b6250d0bd9b1089eb2fac1
Hieu Nguyen <hieunguyen.0527@gmail.com>
2026-05-02 18:17:46 -0400

    fix workshop migration timestamp

---
+++ apps/api/internal/database/migrations/20260502221609_workshops.sql
@@ -0,0 +1,48 @@
+-- +goose Up
+-- +goose StatementBegin
+
+-- TABLES
+create table workshops
+(
+	id uuid default gen_random_uuid() not null primary key,
+	title text not null,
+	description text,
+	start_time timestamptz not null,
+	end_time timestamptz not null,
+	num_attendees integer default 0 not null,
+	location text,
+	presenter text,
+	created_at timestamptz default now() not null,
+	updated_at timestamptz default now() not null
+);
+
+create table workshop_registrations
+(
+    user_id uuid not null,
+    workshop_id uuid not null,
+    created_at timestamptz default now() not null,
+
+    primary key (user_id, workshop_id),
+
+    foreign key (workshop_id)
+        references workshops(id)
+        on delete cascade,
+	foreign key (user_id)
+		references users(id)
+		on delete cascade
+);
+
+-- TRIGGERS
+create trigger set_updated_at_workshops
+	before update
+	on workshops
+	for each row
+	execute procedure update_modified_column();
+
+-- +goose StatementEnd
+-- +goose Down
+drop trigger if exists set_updated_at_workshops on workshops;
+
+drop table if exists workshop_registrations;
+
+drop table if exists workshops;
