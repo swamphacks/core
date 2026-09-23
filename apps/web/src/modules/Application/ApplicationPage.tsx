e66d0700835a1be9a8b2fb66d89d7cbe494cab74
Hieu Nguyen <hieunguyen.0527@gmail.com>
2026-09-23 19:50:12 -0400

    update rejection page

--- apps/web/src/modules/Application/ApplicationPage.tsx
+++ apps/web/src/modules/Application/ApplicationPage.tsx
@@ -195,12 +195,12 @@ function Rejected({ name }: RejectedProps) {
             available closer to the event. You can join the waitlist by signing
             up in person on the day of check-in if space allows. The waitlist
             operates on a first-come, first-served basis. */}
-            1. Join the waitlist: Our waitlist will be coming out soon, and
-            you'll receive more information when they do. You will be able to
-            join the waitlist through this page when it becomes available. You
-            can also join the waitlist by signing up in person on the day of
-            check-in if space allows. The waitlist operates on a first-come,
-            first-served basis.
+            1. <strong>Join the Waitlist</strong>: Our waitlist will be coming
+            out soon, and you'll receive more information when they do. You will
+            be able to join the waitlist through this page when it becomes
+            available. You can also join the waitlist by signing up in person on
+            the day of check-in if space allows. The waitlist operates on a
+            first-come, first-served basis.
           </li>
           <li>
             2. <strong>Mentor</strong>: Share your knowledge and guide hackers
